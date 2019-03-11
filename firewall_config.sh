b45780b95693af15d0d65fd3219e676121095415
arian42 <arian.heydari.42@gmail.com>
2019-03-11 20:49:08 +0330

    add debug

--- firewall_config.sh
+++ firewall_config.sh
@@ -22,10 +22,10 @@ systemctl enable ipset
 ipset create iran_ipv4 hash:net
 
 # Read CIDR file and add them to ipset
-while IFS="" read -r p || [ -n "$p" ]
-do
-  ipset add iran_ipv4 $p
-done < iran_ipv4.csv
+while read line; do
+  echo ipset add iran_ipv4 $line
+done < iran_ipv4.txt
+
 
 # Create firewall ruls
 # {
@@ -33,24 +33,28 @@ done < iran_ipv4.csv
 iptables -t filter -F
 iptables -t filter -X
 
+printf \e[1;33m debug\e[0m 1
 # Set default output to accept because of VPN
 # We don't need forward for softether so drop all
 iptables -t filter -P INPUT DROP
 iptables -t filter -P FORWARD DROP
 iptables -t filter -P OUTPUT ACCEPT
 
+printf \e[1;33m debug\e[0m 2
 # Authorize already established connections. it is important for speed
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -t filter -A INPUT -i lo -j ACCEPT
 iptables -t filter -A OUTPUT -o lo -j ACCEPT
 
+printf \e[1;33m debug\e[0m 3
 # Disable input Ping it also will disable PMTUD. I am not sure about this rule.
 iptables -t filter -A INPUT -p icmp -m state --state NEW -j DROP
 #iptables -t filter -A INPUT -p icmp -m state --state ESTABLISHED,RELATED -j ACCEPT
 #iptables -t filter -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -t filter -A OUTPUT -p icmp -m -j ACCEPT
 
+printf \e[1;33m debug\e[0m 4
 # Access SSH only from Iran 
 iptables -t filter -A INPUT -m set --match-set iran_ipv4 src -p tcp --dport 22 --state new -j ACCEPT
 iptables -t filter -A OUTPUT -m set --match-set iran_ipv4 dst -p tcp --sport 22 --state new -j ACCEPT
@@ -67,6 +71,7 @@ iptables -t filter -A OUTPUT -m set --match-set iran_ipv4 dst -p tcp --sport 22 
 #iptables -A OUTPUT -p tcp -d $dnsip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
 #iptables -A INPUT  -p tcp -s $dnsip --sport 53 -m state --state ESTABLISHED     -j ACCEPT
 
+printf \e[1;33m debug\e[0m 5
 # you only can connect to vpn from iran and no program can start connection to Iran so you can't connect to Iran with vpn
 iptables -t filter -A INPUT -m set --match-set iran_ipv4 src --state new -p tcp --dport 443 -j ACCEPT
 iptables -t filter -A INPUT -m set --match-set iran_ipv4 src --state new -p udp --dport 443 -j ACCEPT
@@ -77,6 +82,7 @@ iptables -t filter -A OUTPUT -m set --match-set iran_ipv4 dst --state new -p udp
 #iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
 # it will accept by default rule. no need to set it.
 
+printf \e[1;33m debug\e[0m 6
 # Log before dropping
 iptables -A INPUT  -j LOG  -m limit --limit 12/min --log-level 4 --log-prefix 'IP INPUT drop: '
 iptables -A INPUT -j DROP
